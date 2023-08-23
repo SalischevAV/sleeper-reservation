11794b100a8f44cc3327cf46739e44b749b36dba
SalischevAV <devorer77@gmail.com>
2023-08-23 11:41:44 +0200

    auth

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { UserDocument } from './models/users.schema';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { CurrentUser } from '@app/common';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUser(@CurrentUser() user: UserDocument) {
+    return user;
+  }
+}
